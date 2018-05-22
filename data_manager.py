7c234d85e96805c97bc0472ba2de69669064b8f7
joe <joeodams@gmail.com>
2018-05-22 22:08:15 +0100

    remove < 0 data points

--- data_manager.py
+++ data_manager.py
@@ -182,6 +182,9 @@ class SwathTileManager:
         #convert to local
         merged_stack["x"] = merged_stack["lat"].apply(lambda x: (x - bounding_box.min_lat) * 100)
         merged_stack["y"] = merged_stack["long"].apply(lambda x: (x - bounding_box.min_long) * 100)
+
+        merged_stack = merged_stack.drop(merged_stack[merged_stack.Cloud_Top_Height < 0].index)
+
         return merged_stack
 
     def get_local_point(self, minimum, point):
