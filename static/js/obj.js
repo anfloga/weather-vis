4e12b22fd741fb13b88909adf93201ca9b7a2787
joe <joeodams@gmail.com>
2018-06-03 14:42:05 +0100

    specify PlaneLayer and PointLayer types

--- static/js/obj.js
+++ static/js/obj.js
@@ -17,19 +17,45 @@ class Point {
 
 class Layer {
     constructor() {
+        this.material = new THREE.MeshBasicMaterial({color: 0xffff00});
         this.geometry = new THREE.Geometry();
         this.mesh = new THREE.Mesh();
     }
 
-    addToMesh(point) {
-        this.geometry.mergeMesh(point.getPointMesh());
-    }
-
     addToScene(scene) {
-        this.mesh = new THREE.Mesh(this.geometry, new THREE.MeshBasicMaterial({color: 0xffff00}));
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
         scene.add(this.mesh);
     }
 }
+
+class PointLayer extends Layer {
+    constructor() {
+        super();
+    }
+
+    addPoint(point) {
+        this.geometry.mergeMesh(point.getPointMesh());
+    }
+}
+
+
+class PlaneLayer extends Layer {
+    constructor(x, y) {
+        super();
+        //this.material = new THREE.MeshPhongMaterial({color: 0xdddddd, wireframe: false});
+        this.geometry = new THREE.PlaneGeometry(x * 400, y * 400, 800, 800);
+    }
+
+    setSegmentHeight(point) {
+        var x = Math.round(point.x);
+        var y = Math.round(point.y);
+        var index = x * y;
+        this.geometry.vertices[index].z = point.dat;
+    }
+}
+
+
+
 
 function getCamera() {
     var camera;
@@ -49,21 +75,32 @@ function render(scene, camera) {
     renderer.render(scene, camera);
 }
 
-async function buildLayer(url) {
-    var layer = new Layer();
+async function buildPointLayer(url) {
+    var layer = new PointLayer();
     var raw = await fetch(url);
     var pointArray = await raw.json();
-    console.log(pointArray);
-    console.log(pointArray[450]);
-    alert(pointArray.length);
 
     for (var key in pointArray) {
         var pointData = pointArray[key];
         var point = new Point(pointData.x, pointData.y, pointData.Cloud_Top_Height);
-        layer.addToMesh(point);
+        layer.addPoint(point);
     }
     layer.addToScene(scene);
 }
+
+async function buildPlaneLayer(url) {
+    var layer = new PlaneLayer();
+    var raw = await fetch(url);
+    var pointArray = await raw.json();
+
+    for (var key in pointArray) {
+        var pointData = pointArray[key];
+        var point = new Point(pointData.x, pointData.y, pointData.Cloud_Top_Height);
+        layer.setSegmentHeight(point);
+    }
+    layer.addToScene(scene);
+}
+
 
 function animate() {
     requestAnimationFrame(animate);
@@ -76,5 +113,5 @@ var renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-buildLayer("http://127.0.0.1:5000/get");
+buildPlaneLayer("http://127.0.0.1:5000/get");
 animate();
